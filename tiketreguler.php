b57a40bbe0bd54b8b844a1ea957851303a88283e
bangkiitdd <bangkittadi@outlook.com>
2026-06-17 09:02:52 +0700

    [Tahap 4] Implementasi pewarisan

--- tiketreguler.php
+++ tiketreguler.php
@@ -1,28 +1,28 @@
 <?php
-// TiketRegular.php
 require_once 'Tiket.php';
 
 class TiketRegular extends Tiket {
-    // Properti tambahan spesifik
+    // Properti tambahan khusus TiketRegular
     private $tipeAudio;
     private $lokasiBaris;
 
+    // Constructor untuk menginisialisasi properti induk dan anak
     public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket, $tipeAudio, $lokasiBaris) {
-        // Memanggil constructor dari abstract class Tiket
-        parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket);
+        $this->id_tiket = $id_tiket;
+        $this->nama_film = $nama_film;
+        $this->jadwal_tayang = $jadwal_tayang;
+        $this->jumlah_kursi = $jumlah_kursi;
+        $this->hargaDasarTiket = $hargaDasarTiket;
         $this->tipeAudio = $tipeAudio;
         $this->lokasiBaris = $lokasiBaris;
     }
 
-    // Implementasi metode abstrak hitungTotalHarga()
+    // Implementasi abstract method dari class induk
     public function hitungTotalHarga() {
-        // Contoh kalkulasi: harga dasar x jumlah kursi
         return $this->hargaDasarTiket * $this->jumlah_kursi;
     }
 
-    // Implementasi metode abstrak tampilkanInfoFasilitas()
     public function tampilkanInfoFasilitas() {
-        return "Fasilitas Studio Regular -> Tipe Audio: " . ($this->tipeAudio ?? '-') . ", Lokasi Baris: " . ($this->lokasiBaris ?? '-');
+        return "Fasilitas Regular: Audio " . $this->tipeAudio . ", Kursi di Baris " . $this->lokasiBaris;
     }
 }
-?>
