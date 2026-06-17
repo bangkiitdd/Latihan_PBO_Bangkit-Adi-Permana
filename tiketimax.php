c7d60df27f5a8a15da8734a408c0efe78d049e32
bangkiitdd <bangkittadi@outlook.com>
2026-06-17 09:00:30 +0700

    [Tahap 4] Implementasi pewarisan

--- tiketimax.php
+++ tiketimax.php
@@ -1,28 +1,28 @@
 <?php
-// TiketIMAX.php
 require_once 'Tiket.php';
 
 class TiketIMAX extends Tiket {
-    // Properti tambahan spesifik
-    private $kacamata3dId;
+    // Properti tambahan khusus TiketIMAX
+    private $kacamata3did;
     private $efekGerakFitur;
 
-    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket, $kacamata3dId, $efekGerakFitur) {
-        parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket);
-        $this->kacamata3dId = $kacamata3dId;
+    // Constructor untuk menginisialisasi properti induk dan anak
+    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket, $kacamata3did, $efekGerakFitur) {
+        $this->id_tiket = $id_tiket;
+        $this->nama_film = $nama_film;
+        $this->jadwal_tayang = $jadwal_tayang;
+        $this->jumlah_kursi = $jumlah_kursi;
+        $this->hargaDasarTiket = $hargaDasarTiket;
+        $this->kacamata3did = $kacamata3did;
         $this->efekGerakFitur = $efekGerakFitur;
     }
 
-    // Implementasi metode abstrak hitungTotalHarga()
+    // Implementasi abstract method dari class induk
     public function hitungTotalHarga() {
-        // Contoh kalkulasi: Ada biaya tambahan flat untuk IMAX per tiket (misal + Rp 15.000)
-        $biayaTambahanIMAX = 15000;
-        return ($this->hargaDasarTiket + $biayaTambahanIMAX) * $this->jumlah_kursi;
+        return $this->hargaDasarTiket * $this->jumlah_kursi;
     }
 
-    // Implementasi metode abstrak tampilkanInfoFasilitas()
     public function tampilkanInfoFasilitas() {
-        return "Fasilitas Studio IMAX -> ID Kacamata 3D: " . ($this->kacamata3dId ?? '-') . ", Efek Gerak: " . ($this->efekGerakFitur ?? '-');
+        return "Fasilitas IMAX: Kacamata 3D ID [" . $this->kacamata3did . "], Fitur Efek Gerak: " . $this->efekGerakFitur;
     }
 }
-?>
